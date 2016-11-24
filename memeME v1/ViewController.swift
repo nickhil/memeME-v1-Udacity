1c2d15fefe0613bc92b03d48e8cd6187354ca3f2
nikhilB <nikhil9313@gmail.com>
2016-11-24 12:16:12 +0530

    Initial Commit

---
+++ memeME v1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  memeME v1
+//
+//  Created by Nikhil on 24/11/16.
+//  Copyright © 2016 Nikhil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
